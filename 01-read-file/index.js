4238f3003a4260280514648eb888bcd755cbc938
Marina Kotova <m.klementyeva@mail.ru>
2023-05-05 13:27:24 +0200

    feat: solve task: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const textPath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(textPath, 'utf-8');
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log(data));

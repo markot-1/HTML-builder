0329b0d4b57caa83982d63188c2f2221f9921bf9
Marina Kotova <m.klementyeva@mail.ru>
2023-05-08 13:10:35 +0200

    feat: solve task: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
+let originalDirPath = path.join(__dirname, 'files');
+let newDirPath = path.join(__dirname, 'files-copy');
+
+fs.mkdir(newDirPath, 
+{ recursive: true }, err => {
+    if(err) throw err;
+});
+
+fs.readdir(originalDirPath,
+    (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                fs.copyFile(originalDirPath + '/' + file, newDirPath + '/' + file, err => {
+                    if(err) throw err; 
+                 });
+            });
+        }
+    });
